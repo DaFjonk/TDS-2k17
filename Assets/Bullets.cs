d853ef4f1913b921a4b8c5206dbeffe88fcd62fe
DaFjonk <Filip.romdala@hotmail.se>
2017-01-20 19:35:13 +0100

    master game

---
+++ Assets/Bullets.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullets : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+    public Rigidbody2D bullet;
+    public Transform Muzzle;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+         
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody2D Shot = Instantiate(bullet, Muzzle.position, transform.rotation) as Rigidbody2D;
+            if (GetComponent<PlayerController>().direction == 5)
+            {
+                Shot.AddForce(Vector3.up * 500);
+            }
+            if (GetComponent<PlayerController>().direction == 10)
+            {
+                Shot.AddForce(Vector3.right * 500);
+            }
+            if (GetComponent<PlayerController>().direction == 15)
+            {
+                Shot.AddForce(Vector3.left * 500);
+            }
+            if (GetComponent<PlayerController>().direction == 20)
+            {
+                Shot.AddForce(Vector3.down * 500);
+            }
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+        float Sprint = 5;
+        public int direction;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Sprint = 10;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * Sprint * Time.deltaTime, relativeTo: Space.World);
+            direction = 5;
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.rotation = Quaternion.AngleAxis(90, Vector3.forward);
+            }          
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * Sprint * Time.deltaTime, relativeTo: Space.World);
+            direction = 10;
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * Sprint * Time.deltaTime, relativeTo: Space.World);
+            direction = 15;
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.rotation = Quaternion.AngleAxis(180, Vector3.forward);
+            }
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * Sprint * Time.deltaTime, relativeTo: Space.World);
+            direction = 20;
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.rotation = Quaternion.AngleAxis(270, Vector3.forward);
+            }
+        }
+    }
+}
